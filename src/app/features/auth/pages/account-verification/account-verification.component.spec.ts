f696074404e7d0ec22a57853e711c2a9a181fcc7
pangaribowo <fatahillahalifp04@gmail.com>
2025-07-07 19:39:00 +0700

    test: fix provider CAPTCHA_CONFIG for ng-hcaptcha in AccountVerificationComponent unit test

--- src/app/features/auth/pages/account-verification/account-verification.component.spec.ts
+++ src/app/features/auth/pages/account-verification/account-verification.component.spec.ts
@@ -3,6 +3,7 @@ import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { provideLocationMocks } from '@angular/common/testing';
+import { CAPTCHA_CONFIG } from 'ng-hcaptcha';
 
 import { AccountVerificationComponent } from './account-verification.component';
 
@@ -17,7 +18,8 @@ describe('AccountVerificationComponent', () => {
         provideRouter([]),
         provideLocationMocks(),
         provideHttpClient(),
-        provideHttpClientTesting()
+        provideHttpClientTesting(),
+        { provide: CAPTCHA_CONFIG, useValue: { siteKey: 'dummy' } }
       ]
     })
     .compileComponents();
